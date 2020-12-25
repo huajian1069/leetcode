cf101559488aa843a1ecfce572af228841262008
huajian1069 <huajian1069@gmail.com>
2020-12-25 10:08:32 +0100

    solve the Algorithm question during ByteDance interview

---
+++ src/0006-b-spiral-printing/b-spiral-printing_test.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+#include "b-spiral-printing.cpp"
+
+TEST(Solution, SimpleTest1){
+	Solution solution;	
+	vector<int> real{1,2,3,8,9,4,7,6,5};
+	auto pred = solution.spiralPrinting(3, 3);
+	ASSERT_EQ(real, pred);
+}
+
+TEST(Solution, SimpleTest2){
+	Solution solution;	
+	vector<int> real{1,2,3,4,5,14,15,16,17,6,13,20,19,18,7,12,11,10,9,8};
+	auto pred = solution.spiralPrinting(4, 5);
+	ASSERT_EQ(real, pred);
+}
