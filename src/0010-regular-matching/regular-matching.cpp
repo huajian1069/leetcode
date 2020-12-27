c0a89bbb38d7b9f6e18419a4c49afadd3a6472d7
huajian1069 <huajian1069@gmail.com>
2020-12-27 19:51:45 +0100

    adapt to DP version: reduce time a lot

--- src/0010-regular-matching/regular-matching.cpp
+++ src/0010-regular-matching/regular-matching.cpp
@@ -4,9 +4,19 @@ using namespace std;
 class Solution {
 public:
     bool isMatch(string s, string p) {
+        if(mem == nullptr){
+            mem = new char*[s.length()+1];
+            for (int i = 0; i <= s.length(); i++){
+                mem[i] = new char[p.length()+1];
+                for(int j = 0; j <= p.length(); j++)
+                    mem[i][j] = 'x';
+            }
+        }
+        if(mem[s.length()][p.length()] != 'x')
+            return mem[s.length()][p.length()];
     	// cout << "s: " << s << endl;
     	// cout << "p: " << p << endl;
-        bool stop = false, match = true;
+        bool match = true;
         int i = 0, j = 0;
     	if(j + 1 < p.length() && p[j+1] == '*'){
     		j += 2;
@@ -27,8 +37,10 @@ public:
                 }
                 else  match = i == s.length();
     		}
+            mem[s.length()][p.length()] = match;
     		return match;
     	}
+
         if(i == s.length() || j == p.length())
             match = i == s.length() && j == p.length();
         else if(s[i] == p[j] || p[j] == '.'){
@@ -48,6 +60,9 @@ public:
         // cout << "j: " << j << endl;
         // cout << "i: " << i << endl;
         // cout << "match: " << match << endl;    
+        mem[s.length()][p.length()] = match;
         return match;
     }
+private:
+    char **mem = nullptr;
 };
