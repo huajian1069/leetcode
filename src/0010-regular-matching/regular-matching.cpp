300b8409001ea9c11f2ad17278a0c64cfca18d86
huajian1069 <huajian1069@gmail.com>
2020-12-27 11:47:09 +0100

    finally pass p10

---
+++ src/0010-regular-matching/regular-matching.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+    	cout << "s: " << s << endl;
+    	cout << "p: " << p << endl;
+        bool stop = false, match = true;
+        int i = 0, j = 0;
+        char aft = ' ';
+        while(!stop){
+        	if(j + 1 < p.length())
+        		aft = p[j+1]; 
+        	if(aft == '*'){
+        		j += 2;
+                if(j < p.length()){
+                    if(i == s.length())
+                        match = isMatch("", p.substr(j, p.length()-j));
+                    else
+                        match = isMatch(s.substr(i, s.length()-i), p.substr(j, p.length()-j));
+                }
+        		else  match = i == s.length();
+        		while(!match && i < s.length() && (s[i] == p[j-2] || p[j-2] == '.')){
+        			i++;
+                    if(j < p.length()){
+                        if(i == s.length())
+                            match = isMatch("", p.substr(j, p.length()-j));
+                        else
+                            match = isMatch(s.substr(i, s.length()-i), p.substr(j, p.length()-j));
+                    }
+                    else  match = i == s.length();
+        		}
+        		return match;
+        	}
+            if(i == s.length() || j == p.length())
+                match = i == s.length() && j == p.length();
+            else if(s[i] == p[j] || p[j] == '.'){
+                i += 1;
+                j += 1;
+            } else match = false;
+            cout << "middle j: " << j << endl;
+       		cout << "middle i: " << i << endl;
+        	cout << "middle match: " << match << endl;  
+            stop = !match || (match && i == s.length() && j == p.length()); 
+        }
+        cout << "j: " << j << endl;
+        cout << "i: " << i << endl;
+        cout << "match: " << match << endl;    
+        return match;
+    }
+};
---
+++ src/0010-regular-matching/regular-matching_test.cpp
@@ -0,0 +1,80 @@
+#include <gtest/gtest.h>
+#include "regular-matching.cpp"
+
+TEST(Solution, SimpleTest1){
+	Solution solution;
+	bool pred = solution.isMatch("aa", "a");
+	ASSERT_EQ(pred, false);
+}
+
+TEST(Solution, SimpleTest2){
+	Solution solution;
+	bool pred = solution.isMatch("aa", "a*");
+	ASSERT_EQ(pred, true);
+}
+
+TEST(Solution, SimpleTest3){
+	Solution solution;
+	bool pred = solution.isMatch("ab", ".*");
+	ASSERT_EQ(pred, true);
+}
+
+TEST(Solution, SimpleTest4){
+	Solution solution;
+	bool pred = solution.isMatch("aab", "c*a*b");
+	ASSERT_EQ(pred, true);
+}
+
+TEST(Solution, SimpleTest5){
+	Solution solution;
+	bool pred = solution.isMatch("mississippi", "mis*is*p*");
+	ASSERT_EQ(pred, false);
+}
+
+TEST(Solution, SimpleTest6){
+	Solution solution;
+	bool pred = solution.isMatch("aaa", "a*a");
+	ASSERT_EQ(pred, true);
+}
+
+TEST(Solution, SimpleTest7){
+	Solution solution;
+	bool pred = solution.isMatch("aaa", "ab*ac*a");
+	ASSERT_EQ(pred, true);
+}
+
+TEST(Solution, SimpleTest8){
+	Solution solution;
+	bool pred = solution.isMatch("ab", ".*c");
+	ASSERT_EQ(pred, false);
+}
+
+TEST(Solution, SimpleTest9){
+	Solution solution;
+	bool pred = solution.isMatch("a", "ab*");
+	ASSERT_EQ(pred, true);
+}
+
+TEST(Solution, SimpleTest10){
+	Solution solution;
+	bool pred = solution.isMatch("b", "b.*c");
+	ASSERT_EQ(pred, false);
+}
+
+TEST(Solution, SimpleTest11){
+	Solution solution;
+	bool pred = solution.isMatch("", "c*c*");
+	ASSERT_EQ(pred, true);
+}
+
+TEST(Solution, SimpleTest12){
+	Solution solution;
+	bool pred = solution.isMatch("", "");
+	ASSERT_EQ(pred, true);
+}
+
+TEST(Solution, SimpleTest13){
+	Solution solution;
+	bool pred = solution.isMatch("abbabaaaaaaacaa", "a*.*b.a.*c*b*a*c*");
+	ASSERT_EQ(pred, true);
+}
