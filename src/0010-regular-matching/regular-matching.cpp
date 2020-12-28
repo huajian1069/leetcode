bc94b96d0b008e7248d0876e475f56270e967c77
huajian1069 <huajian1069@gmail.com>
2020-12-28 23:59:36 +0100

    simplify the recursion case of sultion for P10

--- src/0010-regular-matching/regular-matching.cpp
+++ src/0010-regular-matching/regular-matching.cpp
@@ -20,39 +20,17 @@ public:
         int i = 0, j = 0;
     	if(j + 1 < p.length() && p[j+1] == '*'){
     		j += 2;
-            if(j < p.length()){
-                if(i == s.length())
-                    match = isMatch("", p.substr(j, p.length()-j));
-                else
-                    match = isMatch(s.substr(i, s.length()-i), p.substr(j, p.length()-j));
-            }
-    		else  match = i == s.length();
+            match = isMatch(s.substr(i, s.length()-i), p.substr(j, p.length()-j));
     		while(!match && i < s.length() && (s[i] == p[j-2] || p[j-2] == '.')){
     			i++;
-                if(j < p.length()){
-                    if(i == s.length())
-                        match = isMatch("", p.substr(j, p.length()-j));
-                    else
-                        match = isMatch(s.substr(i, s.length()-i), p.substr(j, p.length()-j));
-                }
-                else  match = i == s.length();
+                match = isMatch(s.substr(i, s.length()-i), p.substr(j, p.length()-j));
     		}
-            mem[s.length()][p.length()] = match;
-    		return match;
-    	}
-
-        if(i == s.length() || j == p.length())
+    	} else if(i == s.length() || j == p.length())
             match = i == s.length() && j == p.length();
         else if(s[i] == p[j] || p[j] == '.'){
             i += 1;
             j += 1;
-            if(j < p.length()){
-                if(i == s.length())
-                    match = isMatch("", p.substr(j, p.length()-j));
-                else
-                    match = isMatch(s.substr(i, s.length()-i), p.substr(j, p.length()-j));
-            }
-            else  match = i == s.length();
+            match = isMatch(s.substr(i, s.length()-i), p.substr(j, p.length()-j));
         } else match = false;
             // cout << "middle j: " << j << endl;
        		// cout << "middle i: " << i << endl;
