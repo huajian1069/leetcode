b581bf97909a617b0b8b58261278f082e64b595e
huajian1069 <huajian1069@gmail.com>
2020-12-30 01:17:42 +0100

    pass the P11: slow solution

---
+++ src/0011-container-with-most-water/container-with-most-water_test.cpp
@@ -0,0 +1,19 @@
+#include <gtest/gtest.h>
+#include "container-with-most-water.cpp"
+
+TEST(Solution, SimpleTest1){
+	vector<int> height(20000);
+	int x = 0;
+	std::generate(height.begin(), height.begin()+10000, [&]{ return x++; }); 
+	std::generate(height.begin()+10000, height.begin()+20000, [&]{ return x--; }); 
+	Solution solution;
+	int pred = solution.maxArea(height);
+	ASSERT_EQ(pred, 50000000);
+}
+
+TEST(Solution, SimpleTest2){
+	vector<int> height{1,8,6,2,5,4,8,3,7};
+	Solution solution;
+	int pred = solution.maxArea(height);
+	ASSERT_EQ(pred, 49);
+}
